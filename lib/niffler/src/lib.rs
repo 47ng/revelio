480fe2e29ddd33b873cd9f7b0175941935182b04
Francois Best <francois@francoisbest.com>
2019-02-21 10:08:12 +0100

    feat: Add tests

--- lib/niffler/src/lib.rs
+++ lib/niffler/src/lib.rs
@@ -129,3 +129,123 @@ pub fn detect() -> Option<BuildInfo> {
   }
   None
 }
+
+// -----------------------------------------------------------------------------
+
+#[test]
+fn travis() {
+  std::env::set_var("CI", "true");
+  std::env::set_var("TRAVIS", "true");
+  let info = detect();
+  assert!(info.is_none()); // Missing required environment variables
+
+  std::env::set_var(
+    "TRAVIS_JOB_WEB_URL",
+    "https://travis-ci.com/foo/bar/jobs/42",
+  );
+  std::env::set_var("TRAVIS_COMMIT", "cafebabe");
+  std::env::set_var("TRAVIS_REPO_SLUG", "egg/spam");
+  std::env::set_var("TRAVIS_COMMIT_RANGE", "aa...ff");
+
+  let info = detect();
+  assert!(info.is_some());
+  let info = info.unwrap();
+
+  assert_eq!(info.commit_sha1, "cafebabe");
+  assert_eq!(info.build_url, "https://travis-ci.com/foo/bar/jobs/42");
+  assert_eq!(info.sources_url, "https://github.com/egg/spam");
+  assert_eq!(
+    info.compare_url,
+    "https://github.com/egg/spam/compare/aa...ff"
+  );
+  assert_eq!(
+    info.commit_url,
+    "https://github.com/egg/spam/commit/cafebabe"
+  );
+
+  // Cleanup
+  std::env::remove_var("CI");
+  std::env::remove_var("TRAVIS");
+  std::env::remove_var("TRAVIS_JOB_WEB_URL");
+  std::env::remove_var("TRAVIS_COMMIT");
+  std::env::remove_var("TRAVIS_REPO_SLUG");
+  std::env::remove_var("TRAVIS_COMMIT_RANGE");
+}
+
+// -----------------------------------------------------------------------------
+
+#[test]
+fn circle_github() {
+  std::env::set_var("CI", "true");
+  std::env::set_var("CIRCLECI", "true");
+  let info = detect();
+  assert!(info.is_none()); // Missing required environment variables
+
+  std::env::set_var("CIRCLE_BUILD_URL", "https://circleci.com/gh/foo/bar/42");
+  std::env::set_var("CIRCLE_SHA1", "facade42");
+  std::env::set_var("CIRCLE_REPOSITORY_URL", "git@github.com:baz/qux.git");
+  std::env::set_var("CIRCLE_PROJECT_USERNAME", "egg");
+  std::env::set_var("CIRCLE_PROJECT_REPONAME", "spam");
+  std::env::set_var("CIRCLE_COMPARE_URL", "https://example.com/compare/aa...ff");
+
+  let info = detect();
+  assert!(info.is_some());
+  let info = info.unwrap();
+  assert_eq!(info.commit_sha1, "facade42");
+  assert_eq!(info.build_url, "https://circleci.com/gh/foo/bar/42");
+  assert_eq!(info.sources_url, "https://github.com/egg/spam");
+  assert_eq!(info.compare_url, "https://example.com/compare/aa...ff");
+  assert_eq!(
+    info.commit_url,
+    "https://github.com/egg/spam/commit/facade42"
+  );
+
+  // Cleanup
+  std::env::remove_var("CI");
+  std::env::remove_var("CIRCLECI");
+  std::env::remove_var("CIRCLE_BUILD_URL");
+  std::env::remove_var("CIRCLE_SHA1");
+  std::env::remove_var("CIRCLE_REPOSITORY_URL");
+  std::env::remove_var("CIRCLE_PROJECT_USERNAME");
+  std::env::remove_var("CIRCLE_PROJECT_REPONAME");
+  std::env::remove_var("CIRCLE_COMPARE_URL");
+}
+
+// -----------------------------------------------------------------------------
+
+#[test]
+fn circle_bitbucket() {
+  std::env::set_var("CI", "true");
+  std::env::set_var("CIRCLECI", "true");
+  let info = detect();
+  assert!(info.is_none()); // Missing required environment variables
+
+  std::env::set_var("CIRCLE_BUILD_URL", "https://circleci.com/bb/foo/bar/42");
+  std::env::set_var("CIRCLE_SHA1", "facade42");
+  std::env::set_var("CIRCLE_REPOSITORY_URL", "git@bitbucket.org:baz/qux.git");
+  std::env::set_var("CIRCLE_PROJECT_USERNAME", "egg");
+  std::env::set_var("CIRCLE_PROJECT_REPONAME", "spam");
+  std::env::set_var("CIRCLE_COMPARE_URL", "https://example.com/compare/aa...ff");
+
+  let info = detect();
+  assert!(info.is_some());
+  let info = info.unwrap();
+  assert_eq!(info.commit_sha1, "facade42");
+  assert_eq!(info.build_url, "https://circleci.com/bb/foo/bar/42");
+  assert_eq!(info.sources_url, "https://bitbucket.org/egg/spam");
+  assert_eq!(info.compare_url, "https://example.com/compare/aa...ff");
+  assert_eq!(
+    info.commit_url,
+    "https://bitbucket.org/egg/spam/commits/facade42"
+  );
+
+  // Cleanup
+  std::env::remove_var("CI");
+  std::env::remove_var("CIRCLECI");
+  std::env::remove_var("CIRCLE_BUILD_URL");
+  std::env::remove_var("CIRCLE_SHA1");
+  std::env::remove_var("CIRCLE_REPOSITORY_URL");
+  std::env::remove_var("CIRCLE_PROJECT_USERNAME");
+  std::env::remove_var("CIRCLE_PROJECT_REPONAME");
+  std::env::remove_var("CIRCLE_COMPARE_URL");
+}
