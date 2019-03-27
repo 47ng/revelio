4266b408c3d4dbec258a365aeb5a5d9a5da315ef
Francois Best <francois@francoisbest.com>
2019-03-27 12:55:43 +0100

    fix: Support <1.32 re-export syntax
    For Now builder that runs on 1.31

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,8 @@ mod reducto;
 pub mod url;
 
 // Aliases & re-exports
-pub use commit::find_commit_author;
-pub use context::Context;
-pub use hash::*;
-pub use manifest::Manifest;
-pub use reducto::*;
+pub use self::commit::find_commit_author;
+pub use self::context::Context;
+pub use self::hash::*;
+pub use self::manifest::Manifest;
+pub use self::reducto::*;
